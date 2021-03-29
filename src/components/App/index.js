34b869f74f642722dbd84fb2c6db3c37d24d718a
Julien Moirenc <julien.moirenc@oclock.io>
2021-03-29 10:02:37 +0200

    choix des composant pour notre app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,17 +10,14 @@ import './styles.css';
 const App = () => (
   <div className="app">
     <header>
-      <h1>Crêpes raffinées</h1>
+      Composant header
     </header>
-    <ul>
-      <li>Ingédient 1</li>
-      <li>Ingédient 2</li>
-    </ul>
-    <ul>
-      <li>Etape 1</li>
-      <li>Etape 2</li>
-      <li>Etape 3</li>
-    </ul>
+    <div>
+      Composant Ingredients
+    </div>
+    <div>
+      Composant Steps
+    </div>
   </div>
 );
 
