c19618a7fcb270a44283bb29c392015f73bc503d
Julien Moirenc <julien.moirenc@oclock.io>
2021-03-29 10:32:28 +0200

    Création de la structure du composant Header. Rappel sur la convention de nommage BEM

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,12 @@ import React from 'react';
 // Un composant est une fonction qui renvoit du JSX
 const Header = () => (
   <header className="header">
-    Je suis le composant Header
+    <img src="https://images.pexels.com/photos/53483/strawberries-crepe-dessert-sweet-53483.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Crêpes raffinées" className="header__image" />
+
+    <div className="header__content">
+      <h1 className="header__title">Crêpes raffinées</h1>
+      <h2 className="header__infos">John Deuf - Facile</h2>
+    </div>
   </header>
 );
 
